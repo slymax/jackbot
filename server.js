276fc3fb1aae290bb27d7b1b1c536a39d4cff01c
slymax <contact@slymax.com>
2019-07-16 18:22:34 +0200

    add monitor skill

--- server.js
+++ server.js
@@ -5,8 +5,13 @@ const Telegraf = require("telegraf");
 const schedule = require("node-schedule");
 const reminders = new Store(config.store);
 const bot = new Telegraf(config.key);
+const fetch = require("node-fetch");
+const cheerio = require("cheerio");
 const app = require("express")();
+const uuid = require('uuid/v4');
+
 const commands = {};
+const monitors = {};
 
 const skills = {
     decide: ctx => {
@@ -28,12 +33,11 @@ const skills = {
         else ctx.reply(`${minutes.toFixed(2)} minutes`);
     },
     remind: ctx => {
+        const id = uuid();
         const time = ctx.message.text.match(/\((.*?)\)/);
-        const id = Math.random().toString(36).substr(2,9);
         const once = time[1].includes(":");
         const reminder = {
             once: once,
-            chat: ctx.chat.id,
             date: once ? moment(time[1], config.remind.format).toDate() : time[1],
             text: ctx.message.text.replace(time[0],"").trim()
         }
@@ -42,27 +46,46 @@ const skills = {
             ctx.reply(reminder.text);
         });
         reminders.set(id, reminder);
+        ctx.reply("Reminder set for " + time);
+    },
+    monitor: async () => {
+        for (site of config.monitor.sites) {
+            const html = await fetch(site.url);
+            const text = await html.text();
+            const $ = cheerio.load(text);
+            const content = $(site.selector || "body").text().replace(/\s+/g, " ").trim();
+            if (monitors[site.url] && monitors[site.url] !== content) {
+                if (!site.keyword || content.toLowerCase().includes(site.keyword.toLowerCase())) {
+                    bot.telegram.sendMessage(config.chat, "Changes found on " + site.url);
+                }
+            }
+            monitors[site.url] = content;
+        }
     }
 }
 
 bot.on("message", ctx => {
-    if (config.users && !config.users.includes(ctx.chat.id)) return;
-    if (ctx.message.entities && ctx.message.entities[0].type === "bot_command") {
-        commands[ctx.chat.id] = ctx.message.text.substr(1);
-    } else if (commands[ctx.chat.id] && skills[commands[ctx.chat.id]]) {
-        skills[commands[ctx.chat.id]](ctx);
+    if (config.chat === ctx.chat.id) {
+        if (ctx.message.entities && ctx.message.entities[0].type === "bot_command") {
+            commands[ctx.chat.id] = ctx.message.text.substr(1);
+        } else if (commands[ctx.chat.id] && skills[commands[ctx.chat.id]]) {
+            skills[commands[ctx.chat.id]](ctx);
+        }
     }
 }).startPolling();
 
 app.get(`/${config.push.path}/:text`, (request, response) => {
-    bot.telegram.sendMessage(config.push.chat, request.params.text);
+    bot.telegram.sendMessage(config.chat, request.params.text);
     response.end();
 }).listen(config.push.port);
 
 for (id in reminders.data) {
     const reminder = reminders.data[id];
     schedule.scheduleJob(reminder.date, () => {
-        bot.telegram.sendMessage(reminder.chat, reminder.text);
+        bot.telegram.sendMessage(config.chat, reminder.text);
         if (reminder.once) reminders.del(id);
     });
 }
+
+schedule.scheduleJob(config.monitor.schedule, skills.monitor);
+skills.monitor();
